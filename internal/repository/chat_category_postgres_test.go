d0aa9d8eaef3e0bbc74bae9eccfbc51eac7fd9a7
Yuriy Kurnosov <yuriy.kurnosov@mvideo.ru>
2025-02-24 09:52:07 +0300

    Рефакторинг тестов для ChatCategoryPostgres

--- internal/repository/chat_category_postgres_test.go
+++ internal/repository/chat_category_postgres_test.go
@@ -1,23 +1,19 @@
 package repository
 
 import (
-	"errors"
-	"fmt"
+	"github.com/Koderbek/pocket_news_bot/internal/config"
 	"github.com/Koderbek/pocket_news_bot/internal/model"
+	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
-	sqlmock "github.com/zhashkevych/go-sqlxmock"
-	"log"
 	"testing"
 )
 
 func TestChatCategoryPostgres_Create(t *testing.T) {
-	db, mock, err := sqlmock.Newx()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer db.Close()
+	godotenv.Load("../../.env") // Загружаем переменные окружения из .env
+	cfg, _ := config.Init(true)
+	db, _ := NewPostgresTestDB(cfg.Db)
 	r := NewChatCategoryPostgres(db)
+	defer db.Close()
 
 	type paramType struct {
 		chatId     int64
@@ -26,34 +22,29 @@ func TestChatCategoryPostgres_Create(t *testing.T) {
 	}
 
 	testCases := []struct {
-		name         string
-		param        paramType
-		shouldFail   bool
-		mockBehavior func()
+		name       string
+		param      paramType
+		shouldFail bool
 	}{
 		{
 			name:       "case-1: valid result",
+			param:      paramType{1, 1, "TestChat"},
 			shouldFail: false,
-			param:      paramType{1, 1, "world"},
-			mockBehavior: func() {
-				query := fmt.Sprintf("INSERT INTO %s", chatCategoryTable)
-				mock.ExpectExec(query).WillReturnResult(sqlmock.NewResult(1, 1))
-			},
 		},
 		{
-			name:       "case-2: return error",
+			name:       "case-2: return error - duplication",
+			param:      paramType{1, 1, "TestChat"},
 			shouldFail: true,
-			param:      paramType{1, 1, "world"},
-			mockBehavior: func() {
-				query := fmt.Sprintf("INSERT INTO %s", chatCategoryTable)
-				mock.ExpectExec(query).WillReturnError(errors.New("return error"))
-			},
+		},
+		{
+			name:       "case-3: return error - no category",
+			param:      paramType{1, 111, "TestChat"},
+			shouldFail: true,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			tc.mockBehavior()
 			err := r.Create(tc.param.chatId, tc.param.categoryId, tc.param.name)
 			if tc.shouldFail {
 				assert.Error(t, err)
@@ -64,189 +55,86 @@ func TestChatCategoryPostgres_Create(t *testing.T) {
 	}
 }
 
-func TestChatCategoryPostgres_Delete(t *testing.T) {
-	db, mock, err := sqlmock.Newx()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer db.Close()
-	r := NewChatCategoryPostgres(db)
-
-	type paramType struct {
-		chatId     int64
-		categoryId int8
-	}
-
-	testCases := []struct {
-		name         string
-		param        paramType
-		shouldFail   bool
-		mockBehavior func()
-	}{
-		{
-			name:       "case-1: valid result",
-			param:      paramType{1, 1},
-			shouldFail: false,
-			mockBehavior: func() {
-				query := fmt.Sprintf("DELETE FROM %s", chatCategoryTable)
-				mock.ExpectExec(query).WillReturnResult(sqlmock.NewResult(1, 1))
-			},
-		},
-		{
-			name:       "case-2: return error",
-			param:      paramType{1, 1},
-			shouldFail: true,
-			mockBehavior: func() {
-				query := fmt.Sprintf("DELETE FROM %s", chatCategoryTable)
-				mock.ExpectExec(query).WillReturnError(errors.New("return error"))
-			},
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			tc.mockBehavior()
-			err := r.Delete(tc.param.chatId, tc.param.categoryId)
-			if tc.shouldFail {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-			}
-		})
-	}
-}
-
 func TestChatCategoryPostgres_GetByChatId(t *testing.T) {
-	db, mock, err := sqlmock.Newx()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer db.Close()
+	godotenv.Load("../../.env") // Загружаем переменные окружения из .env
+	cfg, _ := config.Init(true)
+	db, _ := NewPostgresTestDB(cfg.Db)
 	r := NewChatCategoryPostgres(db)
+	defer db.Close()
+
 	result := []model.ChatCategory{
-		{ChatId: 1, ChatName: "test", CategoryId: 1, CategoryCode: "world", CategoryName: "Мир"},
-		{ChatId: 1, ChatName: "test", CategoryId: 2, CategoryCode: "sport", CategoryName: "Спорт"},
+		{ChatId: 1, ChatName: "TestChat", CategoryId: 1, CategoryCode: "general", CategoryName: "Главное"},
 	}
 
 	testCases := []struct {
-		name         string
-		param        int64
-		result       []model.ChatCategory
-		shouldFail   bool
-		mockBehavior func(chatCategories []model.ChatCategory)
+		name   string
+		param  int64
+		result []model.ChatCategory
 	}{
 		{
-			name:       "case-1: valid result",
-			param:      1,
-			result:     result,
-			shouldFail: false,
-			mockBehavior: func(chatCategories []model.ChatCategory) {
-				query := fmt.Sprintf("SELECT (.+) FROM %s (.+);", chatCategoryTable)
-				rows := sqlmock.NewRows([]string{"chat_id", "chat_name", "category_id", "category_code", "category_name"})
-				for _, cat := range chatCategories {
-					rows.AddRow(cat.ChatId, cat.ChatName, cat.CategoryId, cat.CategoryCode, cat.CategoryName)
-				}
-
-				mock.ExpectQuery(query).WillReturnRows(rows)
-			},
+			name:   "case-1: valid result",
+			param:  1,
+			result: result,
 		},
 		{
-			name:       "case-2: return error",
-			param:      1,
-			result:     result,
-			shouldFail: true,
-			mockBehavior: func(chatCategories []model.ChatCategory) {
-				query := fmt.Sprintf("SELECT (.+) FROM %s (.+);", chatCategoryTable)
-				mock.ExpectQuery(query).WillReturnError(errors.New("return error"))
-			},
+			name:   "case-2: empty result",
+			param:  111,
+			result: nil,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			tc.mockBehavior(tc.result)
 			got, err := r.GetByChatId(tc.param)
-			if tc.shouldFail {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-				assert.Equal(t, tc.result, got)
-			}
+			assert.NoError(t, err)
+			assert.Equal(t, tc.result, got)
 		})
 	}
 }
 
 func TestChatCategoryPostgres_GetByCategoryId(t *testing.T) {
-	db, mock, err := sqlmock.Newx()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer db.Close()
+	godotenv.Load("../../.env") // Загружаем переменные окружения из .env
+	cfg, _ := config.Init(true)
+	db, _ := NewPostgresTestDB(cfg.Db)
 	r := NewChatCategoryPostgres(db)
+	defer db.Close()
+
 	result := []model.ChatCategory{
-		{ChatId: 1, ChatName: "test", CategoryId: 1, CategoryCode: "world", CategoryName: "Мир"},
-		{ChatId: 2, ChatName: "test_2", CategoryId: 1, CategoryCode: "world", CategoryName: "Мир"},
+		{ChatId: 1, ChatName: "TestChat", CategoryId: 1, CategoryCode: "general", CategoryName: "Главное"},
 	}
 
 	testCases := []struct {
-		name         string
-		param        int8
-		result       []model.ChatCategory
-		shouldFail   bool
-		mockBehavior func(chatCategories []model.ChatCategory)
+		name   string
+		param  int8
+		result []model.ChatCategory
 	}{
 		{
-			name:       "case-1: valid result",
-			param:      1,
-			result:     result,
-			shouldFail: false,
-			mockBehavior: func(chatCategories []model.ChatCategory) {
-				query := fmt.Sprintf("SELECT (.+) FROM %s (.+);", chatCategoryTable)
-				rows := sqlmock.NewRows([]string{"chat_id", "chat_name", "category_id", "category_code", "category_name"})
-				for _, cat := range chatCategories {
-					rows.AddRow(cat.ChatId, cat.ChatName, cat.CategoryId, cat.CategoryCode, cat.CategoryName)
-				}
-
-				mock.ExpectQuery(query).WillReturnRows(rows)
-			},
+			name:   "case-1: valid result",
+			param:  1,
+			result: result,
 		},
 		{
-			name:       "case-2: return error",
-			param:      1,
-			result:     result,
-			shouldFail: true,
-			mockBehavior: func(chatCategories []model.ChatCategory) {
-				query := fmt.Sprintf("SELECT (.+) FROM %s (.+);", chatCategoryTable)
-				mock.ExpectQuery(query).WillReturnError(errors.New("return error"))
-			},
+			name:   "case-2: empty result",
+			param:  111,
+			result: nil,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			tc.mockBehavior(tc.result)
 			got, err := r.GetByCategoryId(tc.param)
-			if tc.shouldFail {
-				assert.Error(t, err)
-			} else {
-				assert.NoError(t, err)
-				assert.Equal(t, tc.result, got)
-			}
+			assert.NoError(t, err)
+			assert.Equal(t, tc.result, got)
 		})
 	}
 }
 
 func TestChatCategoryPostgres_HasChatCategory(t *testing.T) {
-	db, mock, err := sqlmock.Newx()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer db.Close()
+	godotenv.Load("../../.env") // Загружаем переменные окружения из .env
+	cfg, _ := config.Init(true)
+	db, _ := NewPostgresTestDB(cfg.Db)
 	r := NewChatCategoryPostgres(db)
+	defer db.Close()
 
 	type paramType struct {
 		chatId     int64
@@ -254,51 +142,72 @@ func TestChatCategoryPostgres_HasChatCategory(t *testing.T) {
 	}
 
 	testCases := []struct {
-		name         string
-		param        paramType
-		result       bool
-		mockBehavior func()
+		name   string
+		param  paramType
+		result bool
 	}{
 		{
 			name:   "case-1: find",
 			param:  paramType{1, 1},
 			result: true,
-			mockBehavior: func() {
-				query := fmt.Sprintf("SELECT category_id FROM %s", chatCategoryTable)
-				rows := sqlmock.NewRows([]string{"category_id"})
-				rows.AddRow(1)
-
-				mock.ExpectQuery(query).WillReturnRows(rows)
-			},
 		},
 		{
-			name:   "case-2: not found",
-			param:  paramType{1, 1},
+			name:   "case-2: not found - no chat",
+			param:  paramType{111, 1},
 			result: false,
-			mockBehavior: func() {
-				query := fmt.Sprintf("SELECT category_id FROM %s", chatCategoryTable)
-				rows := sqlmock.NewRows([]string{"category_id"})
-				rows.AddRow(0)
-
-				mock.ExpectQuery(query).WillReturnRows(rows)
-			},
 		},
 		{
-			name:   "case-3: return error",
-			param:  paramType{1, 1},
+			name:   "case-3: not found - no category",
+			param:  paramType{1, 111},
 			result: false,
-			mockBehavior: func() {
-				query := fmt.Sprintf("SELECT category_id FROM %s", chatCategoryTable)
-				mock.ExpectQuery(query).WillReturnError(errors.New("return error"))
-			},
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			tc.mockBehavior()
 			got := r.HasChatCategory(tc.param.chatId, tc.param.categoryId)
 			assert.Equal(t, tc.result, got)
+		})
+	}
+}
+
+func TestChatCategoryPostgres_Delete(t *testing.T) {
+	godotenv.Load("../../.env") // Загружаем переменные окружения из .env
+	cfg, _ := config.Init(true)
+	db, _ := NewPostgresTestDB(cfg.Db)
+	r := NewChatCategoryPostgres(db)
+	defer db.Close()
+
+	type paramType struct {
+		chatId     int64
+		categoryId int8
+	}
+
+	testCases := []struct {
+		name  string
+		param paramType
+	}{
+		{
+			name:  "case-1: successful delete",
+			param: paramType{1, 1},
+		},
+		{
+			name:  "case-2: no chat",
+			param: paramType{111, 1},
+		},
+		{
+			name:  "case-3: no category",
+			param: paramType{1, 111},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := r.Delete(tc.param.chatId, tc.param.categoryId)
+			assert.NoError(t, err)
+
+			res := r.HasChatCategory(tc.param.chatId, tc.param.categoryId)
+			assert.Equal(t, res, false)
 		})
 	}
 }
