69fbc796e9440beb0ec1e7bf6f7871523533d4b1
Yuriy Kurnosov <yuriy.kurnosov@mvideo.ru>
2025-03-11 10:00:03 +0300

    Изменил формирование сообщения

--- internal/telegram/handlers.go
+++ internal/telegram/handlers.go
@@ -93,10 +93,10 @@ func (b *Bot) chatCategoryButtons(chatId int64) (*tgbotapi.InlineKeyboardMarkup,
 
 	var buttons [][]tgbotapi.InlineKeyboardButton
 	for _, cat := range categories {
-		btnName := cat.Name
+		btnName := "🔲 " + cat.Name
 		for _, chatCat := range chatCategories {
 			if chatCat.CategoryId == cat.Id {
-				btnName = "✅ " + cat.Name
+				btnName = "☑️ " + cat.Name
 				break
 			}
 		}
